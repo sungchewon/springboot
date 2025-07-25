73737e5af13c325299c62764fc652e5eedb9136e
성채원 <chewon0806@gmail.com>
2025-07-25 10:17:42 +0900

    0725 커밋

--- SpringBootBoard/src/main/java/com/smhrd/board/controller/BoardController.java
+++ SpringBootBoard/src/main/java/com/smhrd/board/controller/BoardController.java
@@ -1,6 +1,5 @@
 package com.smhrd.board.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,8 +16,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.smhrd.board.config.BucketConfig;
 import com.smhrd.board.config.FileUploadConfig;
-import com.smhrd.board.config.WebConfig;
 import com.smhrd.board.entity.BoardEntity;
 import com.smhrd.board.entity.UserEntity;
 import com.smhrd.board.service.BoardService;
@@ -29,71 +33,46 @@ import jakarta.servlet.http.HttpSession;
 @RequestMapping("/board") // controller에 requestmapping 진행 시 default url 변경
 public class BoardController {
 
-    private final WebConfig webConfig;
+
 	@Autowired
 	BoardService boardService;
 	
-
+    private final BucketConfig bucketConfig;
+    private final AmazonS3 amazonS3;
     private final FileUploadConfig fileUploadConfig;
 
-    BoardController(FileUploadConfig fileUploadConfig, WebConfig webConfig) {
+    BoardController(FileUploadConfig fileUploadConfig, BucketConfig bucketConfig,
+    				AmazonS3 amazonS3) {
         this.fileUploadConfig = fileUploadConfig;
-        this.webConfig = webConfig;
+        this.amazonS3 = amazonS3;
+        this.bucketConfig = bucketConfig;
     }
 	
 	// 글쓰기 기능
 	@PostMapping("/write")
 	public String write(@RequestParam String title, @RequestParam String content,
 						HttpSession session, @RequestParam MultipartFile image) {
-		// 필요한거 --> 제목, 작성자, 내용, 이미지 (번호, 작성일 --> 생략)
-		// 작성자 -> session에 담긴 값을 가지고 오는 방법
-		// 이미지 -> 이미지 파일을 가지고 와서 서버에 저장
-		//  --> 이미지 경로를 DB에 저장하기 위해 
-		// ㄴ 이미지를 서버에 저장(이미지 저장을 위한 환경설정 코드)
 		String imgPath = "";
 		
 		if(!image.isEmpty()) {
-			// 이미지의 이름 
 			String img_name = image.getOriginalFilename();
-			// java 안에 고유 번호를 만드는 객체 -- UUID
-			// 이미지의 고유 이름 부여
 			
-			String file_name = UUID.randomUUID() + "_" +img_name;
-			// random값_이미지 이름
+			String file_name = UUID.randomUUID() + "_" +img_name;	
 			
-			// C:/upload 폴더에 저장 할 예정
-			// --> 업로드 할 경로를 변수로 가지고 오기  
-			String uploadDir = fileUploadConfig.getUploadDir();
-			
-			// 예시 - C:/upload/123_1.jpg로 저장
-			// filePaht : 실제 이미지가 저장되는 위치를 나타내는 것
-			String filePath = Paths.get(uploadDir,file_name).toString();
-			// uploadDir + file_name으로 작성시 OS에 따라 경로 못잡기 때문에 윗줄 처럼 작성
-			
-			
-			// 파일 경로 확인 후 이미지 저장
 			try {
-				image.transferTo(new File(filePath));
-				// filePath로 이미지 저장
-				
-				// 경로를 별도의 변수에 저장
-				// imgPath : 이미가 저장되는 위치를 데이터 베이스에 저장하는 것
-				imgPath = "/uploads/" + file_name;
-				
-			} catch (IllegalStateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
+				 ObjectMetadata metadata = new ObjectMetadata();
+			     metadata.setContentLength(image.getSize());
+			     metadata.setContentType(image.getContentType());
+
+			     PutObjectRequest request = new PutObjectRequest(bucketConfig.getbucketName(), file_name, image.getInputStream(), metadata)
+			             .withCannedAcl(CannedAccessControlList.PublicRead); // public 접근 허용
+
+			     amazonS3.putObject(request);
+			     imgPath = amazonS3.getUrl(bucketConfig.getbucketName(), file_name).toString();
+			}catch(Exception e) {
 				e.printStackTrace();
 			}
-	
 		}
-		// DB저장 
-		// service 객체를 통해
-		// BoardService -> BoardRepository
-		// save()
-		
 		BoardEntity entity = new BoardEntity();
 		entity.setTitle(title);
 		entity.setContent(content);
